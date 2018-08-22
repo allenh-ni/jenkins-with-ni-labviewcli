f18ab959d5f665e435d7389e1db42079b4d4f46f
AH <ah@fake.com>
2018-08-22 10:17:15 -0500

    adding VIPB file

--- vars/nicli-VIAnalyzerTask.groovy
+++ vars/nicli-VIAnalyzerTask.groovy
@@ -5,6 +5,6 @@ def call(via_configpath, via_reportpath, reportsavetype, lv_exe_filepath, lv_por
 //lv_exe_filepath - Specifies the LabVIEW version to use to run the operation.  This argument is required on macOS and Linux but optional on Windows.  If this argument is left blank ("") on Windows, the LabVIEWCLI will use the most recent version of LabVIEW used on the machine.
 //lv_portnumber - Specifies the port of the VI server (Tools»Options»VI Server»TCP/IP). If this argument is left blank (""), this command will default to 3363.
 
-        echo 'Running VI Analyzer task - \"${via_configpath}\"...'
+        echo "Running VI Analyzer task - \"${via_configpath}\"..."
         bat "LabVIEWCLI -LabVIEWPath \"${lv_exe_filepath}\" -PortNumber \"${lv_portnumber}\" -OperationName RunVIAnalyzer -ConfigPath \"${via_configpath}\" -ReportPath \"${via_reportpath}\" -ReportSaveType \"${via_reportsavetype}\""
 }
